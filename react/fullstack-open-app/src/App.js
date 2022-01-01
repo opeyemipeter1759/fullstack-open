e3bae0a7c67e1e66a9738e844f37b6c93ed758b5
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2022-01-02 00:15:55 +0100

    task 1

--- react/fullstack-open-app/src/App.js
+++ react/fullstack-open-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Contents from "./Contents";
+import Header from "./Header";
+import Total from "./Total";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+    const course = 'Half Stack application development';
+    const part1 = 'Fundamentals of React';
+    const exercises1 = 10;
+    const part2 = 'Using props to pass data';
+    const exercises2 = 7;
+    const part3 = 'State of a component';
+    const exercises3 = 14;
+    return (
+        <div>
+            <Header course={course} />
+            <Contents part1={part1}
+                exercises1={exercises1}
+                part2={part2}
+                exercises2={exercises2}
+                part3={part3}
+                exercises3={exercises3} />
+            <Total
+                exercises1={exercises1}
+                exercises2={exercises2}
+                exercises3={exercises3} />
+       
+        </div>
+    );
 }
 
 export default App;
---
+++ react/fullstack-open-app/src/Total.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Total = ({exercises3,exercises2,exercises1}) => {
+    return <p>number of exercises: {exercises1 + exercises2 + exercises3}</p>;
+}
+
+export default Total
+{}
