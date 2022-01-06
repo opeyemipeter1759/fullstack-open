f6ffefee957f939d1e7aa413dea8b7fe33044c58
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2022-01-07 00:10:31 +0100

    implemented objects

--- react/fullstack-open-app/src/App.js
+++ react/fullstack-open-app/src/App.js
@@ -4,32 +4,31 @@ import Total from "./Total";
 
 function App()
 {
-    const course = 'Half Stack application development';
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    } 
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises : 14
-    }
-    const part3 = {
-        name: "State of a component",
-        exercises: 14
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            }
+            , {
+                name: 'Using props to pass data',
+                exercises: 14
+            },
+            {
+                name: "State of a component",
+                exercises: 14
+            }
+        ]
     }
     return (
         <div>
             <Header course={course} />
-            <Contents part1={part1.name}
-                exercises1={part1.exercises}
-                part2={part2.name}
-                exercises2={part2.exercises}
-                part3={part3.name}
-                exercises3={part3.exercises} />
+            <Contents part1={course.parts}
+             />
             <Total
-                exercises1={part1.exercises}
-                exercises2={part2.exercises}
-                exercises3={part3.exercises} />
+                parts={course.parts}
+               />
        
         </div>
     );
