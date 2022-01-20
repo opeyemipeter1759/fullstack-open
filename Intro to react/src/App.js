2b549c8ee98e906e4bf4f29990fc658b258d9f3d
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2022-01-20 11:45:31 +0100

    A more complex space in react

--- Intro to react/src/App.js
+++ Intro to react/src/App.js
@@ -5,35 +5,48 @@ import Total from "./Total";
 import Hello from "./Hello";
 
 function App() {
-    const name = "peter";
-    const age = 30;
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            }
-            , {
-                name: 'Using props to pass data',
-                exercises: 14
-            },
-            {
-                name: "State of a component",
-                exercises: 14
-            }
-        ]
-    }
-    const [counter, setCounter] = useState(0);
+    // const name = "peter";
+    // const age = 30;
+    // const course = {
+    //     name: 'Half Stack application development',
+    //     parts: [
+    //         {
+    //             name: 'Fundamentals of React',
+    //             exercises: 10
+    //         }
+    //         , {
+    //             name: 'Using props to pass data',
+    //             exercises: 14
+    //         },
+    //         {
+    //             name: "State of a component",
+    //             exercises: 14
+    //         }
+    //     ]
+    // }
+    // const [counter, setCounter] = useState(0);
 
     // setInterval(
     //     () => setCounter(counter + 1), 1000
     // )
-
-    console.log(counter);
-    const handleClick = () => {
-        setCounter(counter + 1 )
+    // console.log( counter );
+    // const handleClick = () =>
+    // {
+    //     setCounter( counter + 1 )
+    // }
+    const [clicks, setClicks] = useState( {
+        left: 0, right: 0
+    } );
+    
+    const handleLeftClick = () =>
+    {
+        const newClicks = {
+            left: clicks.left + 1,
+            right: clicks.right
+        };
+        setClicks( newClicks );
     }
+    
     return (
         <div>
             {/* <Header course={course} />
