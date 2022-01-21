468ff7e3d9d066c92d16ad1748438f139905d3e9
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2022-01-21 04:10:15 +0100

    Tasks 1.6.-1.14. setup

---
+++ IntroToReact/src/Tasks-1.6.-1.14/TaskIndex.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+function TaskIndex()
+{
+    
+    const [good, setGood] = useState( 0 );
+    const [neutral, setNeutral] = useState( 0 );
+    const [bad, setBad] = useState( 0 );
+    return( <div>
+        <h1>Satatics</h1>
+  </div>);
+}
+
+export default TaskIndex;
