fba12831275c8d93d8037a3ce97b898fa0f8a226
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2022-01-22 13:55:58 +0100

    unicafe step3 return nan

--- IntroToReact/src/Tasks-1.6.-1.14/TaskIndex.js
+++ IntroToReact/src/Tasks-1.6.-1.14/TaskIndex.js
@@ -2,57 +2,49 @@ import React, { useState } from "react";
 
 const Statistics = (props) => {
     const { count, text } = props;
-    if ( count === 0 )
-    {
-        return (
-            <p>
-                No feedback given
-            </p>
-        )
-    }
-    else
-    {
-        return (
-            <p>{text} {count} </p>
-
-        )
-    }
+    return (
+        <p>
+            {text} {count}
+        </p>
+    );
 };
-const Button = (props) => {
-    const { text, onClick } = props;
-    return <button onClick={onClick}>{text}</button>;
+const Button = ({text, handleClick}) => {
+    return <button onClick={handleClick}>{text}</button>;
 };
 
 function TaskIndex() {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-    const handleGoodClick = () => {
-        setGood(good + 1);
-    };
-    const handleNeutralClick = () => {
-        setNeutral(neutral + 1);
-    };
-    const handleBadClick = () => {
-        setBad(bad + 1);
-    };
+
+    const handleClicks = ( setValue, value ) =>
+    {
+        setValue( value + 1 )
+    }
+
+    let sum = good + neutral + bad;
+    let average = ( good - bad ) / sum;
+    let positive = good / sum;
+
+
     return (
         <div>
             <h1>give feedback</h1>
-            <Button onClick={handleGoodClick} text="good" />
-            <Button onClick={handleNeutralClick} text="neutral" />
-            <Button onClick={handleBadClick} text="bad" />
+            <Button handleClick={()=>handleClicks( setGood, good)} text="good" />
+            <Button handleClick={()=> handleClicks(setNeutral, neutral)} text="neutral" />
+            <Button handleClick={()=>handleClicks(setBad, bad)} text="bad" />
 
             <h1>Statistics</h1>
             <Statistics count={good} text="good" />
             <Statistics count={neutral} text="neutral" />
             <Statistics count={bad} text="bad" />
-            <Statistics text='all' count = {good + neutral + bad} />
-            <Statistics text="average" count={( good + neutral + bad ) / 3} />
-            <Statistics text="positive" count={good / ( good + neutral + bad ) * 100} />
-            <Statistics text="bad" count={bad / ( good + neutral + bad ) * 100} />
-            <Statistics text="good" count={good / ( good + neutral + bad ) * 100} />
-            <Statistics text="neutral" count={neutral / ( good + neutral + bad ) * 100} />
+            <Statistics text="all" count={sum} />
+            <Statistics text="average" count={average} />
+            <Statistics
+                text="positive"
+                count={positive}
+            />
+       
         </div>
     );
 }
